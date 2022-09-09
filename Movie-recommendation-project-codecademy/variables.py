a738f1fb4e5e52cf208d0c6f786078eed939d858
ShaneFalero <shanefalero@gmail.com>
2022-09-09 13:23:17 -0400

    Added Movie variables (instantiated class)

--- Movie-recommendation-project-codecademy/variables.py
+++ Movie-recommendation-project-codecademy/variables.py
@@ -1,10 +1,10 @@
 #This file is utilized for creating data structure, assigning objects and creating variables to be searched through for the primary program.
 
 class Movie():
-    def __init__(self, title, famous_actors, ratings, cost = 0, subgenre = [] ):
+    def __init__(self, title, ratings, released, cost = 0, subgenre = [] ):
         self.title = title
-        self.actors = famous_actors
         self.ratings = ratings
+        self.released = released
         self.cost = cost
         self.subgenre = subgenre
 
@@ -14,6 +14,7 @@ class Movie():
         Genre: {self.subgenre},
         Ratings: {self.ratings},
         Cost: {self.cost} 
+        Release Date: {self.released}
         ''')
         return
 
@@ -92,5 +93,45 @@ class HashMap:
                 return possible_return_value[1]
 
             retrieval_collisions +=1
-            
+
         return
+
+#Creating variables:
+#Action Movies
+The_Kings_Man = Movie("The King's Man", '6.3/10', 2021, 19.99, ['Action', 'Adventure'])
+LOTR_RotK = Movie('Lord of the Rings: Return of the King', '9.0/10', 2003, 9.99, ['Action', 'Fantasy', 'Adventure'])
+Avengers_Endgame = Movie('Avengers Endgame', '8.4/10', 2019, 19.99, ['Action', 'Sci-fi'] )
+Rogue_One_SW = Movie('Rogue One: A Star Wars Story', '7.8/10', 2016, 19.99  ['Action', 'Sci-fi'])
+The_Bourne_Identity = Movie('The Bourne Identity', '7.9/10', 2002, 14.99 ['Action', 'Thriller'])
+
+
+#Horror Movies
+Scream = Movie('Scream', '6.3/10', 2022, 31.99, ['Horror', 'Thriller'])
+The_Shining = Movie('The Shining', '8.4/10', 1980, 14.99, ['Horror', 'Mystery'])
+The_Conjuring = Movie('The Conjuring', '7.5/10', 2013, 14.99, ['Horror', 'Thriller'] )
+Orphan_First_Kill = Movie('Orphan: First Kill', '6/10', 2022, 24.99, ['Horror', 'Thriller'])
+Us = Movie('Us', '6.8/10', 2019, 14.99, ['Horror', 'Thriller'])
+
+#Science fiction
+Morbius = Movie('Morbius', '5.2/10', 2022, 19.99, ['Action', 'Fantasy', 'Sci-fi'] )
+Avatar = Movie('Avatar', '7.8/10', 2009, 9.99, ['Action, Sci-fi'])
+Interstellar = Movie('Intersteller', '8.6/10', 2014, 9.99, ['Sci-fi', 'Adventure'])
+Ready_Player_One = Movie('Ready Player One', '7.4/10', 2018, 9.99, ['Sci-fi', 'Action'] )
+Inception = Movie('Inception', '8.8/10', 2010, 9.99, ['Action', 'Sci-fi'])
+
+#Comedy
+The_Waterboy = Movie('The Waterboy', '6.1/10', 1998, 7.99, ['Comedy'])
+Dumb_and_dumber = Movie('Dumb and Dumber', '7.3/10', 1992, 9.99, ['Comedy'])
+Shanghai_Knights = Movie('Shanghai Knights', '6.2/10', 2003, 9.99, ['Action', 'Comedy'])
+Free_Guy = Movie('Free Guy', '7.1/10', 2021, 19.99, ['Action', 'Comedy', 'Sci-fi'] )
+Step_Brothers = Movie('Step Brothers', '6.9/10' 2008, 7.99 ['Comedy'])
+
+#Drama
+Gladiator = Movie('Gladiator', '8.5/10', 2000, 12.99, ['Action', 'Adventure', 'Drama'])
+Titanic = Movie('Titanic', '7.9/10', 1997, 12.99, ['Romance', 'Drama'])
+Forrest_Gump = Movie('Forrest Gump', '8.8/10', 1994, 14.99, ['Drama', 'Romance'])
+Django = Movie('Django', '8.4/10', 2012, 12.49 ['Western', 'Drama'])
+The_Queen = Movie('The Queen', '7.3/10' , 2006, 12.99, ['Drama', 'Historical Drama'])
+
+
+
